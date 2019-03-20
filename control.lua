f2f2c2a77e7f1d52da03d588761ec875d82242eb
Alex Safatli <alexs@liberateme.com>
2019-03-20 13:50:56 -0300

    Add entity count/area

--- control.lua
+++ control.lua
@@ -4,13 +4,21 @@ local Area = require('__stdlib__/stdlib/area/area')
 local function measure_area(event)
   if event.item == 'tile-metrics-tool' then
     local player = game.players[event.player_index]
-    local area = Area(event.area)
     if event.name == defines.events.on_player_alt_selected_area then
-      area = area:ceil()
+      local area = Area(event.area)
+      local size, width, height = area:size()
+      player.print(area .. ' Center: ' .. area:center())
+      player.print('Size: ' .. size .. ' Width: ' .. width .. ' Height: ' .. height)
+    else
+      local entities = event.entities
+      local area = Area()
+      for entity in entities do
+        area = area + Area(entity.bounding_box)
+      end
+      local size, width, height = area:size()
+      player.print(area .. ' Center: ' .. area:center() .. ' Num.Entities: ' .. table.getn(entities))
+      player.print('Size: ' .. size .. ' Width: ' .. width .. ' Height: ' .. height)
     end
-    local size, width, height = area:size()
-    player.print(area .. ' Center = ' .. area:center())
-    player.print('Size = ' .. size .. ' Width = ' .. width .. ' Height = ' .. height)
   end
 end
 

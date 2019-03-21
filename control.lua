6383ec73cb360a11491b9a3b5302e5a19df035c6
Alex Safatli <alexs@liberateme.com>
2019-03-21 11:50:33 -0300

    Fix event handler issues, add temp graphic

--- control.lua
+++ control.lua
@@ -1,25 +1,4 @@
-local Event = require('__stdlib__/stdlib/event/event')
-local Area = require('__stdlib__/stdlib/area/area')
+require('__stdlib__/stdlib/core')
+require('__stdlib__/stdlib/event/event').protected_mode = true
 
-local function measure_area(event)
-  if event.item == 'tile-metrics-tool' then
-    local player = game.players[event.player_index]
-    if event.name == defines.events.on_player_alt_selected_area then
-      local area = Area(event.area)
-      local size, width, height = area:size()
-      player.print(area .. ' Center: ' .. area:center())
-      player.print('Size: ' .. size .. ' Width: ' .. width .. ' Height: ' .. height)
-    else
-      local entities = event.entities
-      local area = Area()
-      for entity in entities do
-        area = area + Area(entity.bounding_box)
-      end
-      local size, width, height = area:size()
-      player.print(area .. ' Center: ' .. area:center() .. ' Num.Entities: ' .. table.getn(entities))
-      player.print('Size: ' .. size .. ' Width: ' .. width .. ' Height: ' .. height)
-    end
-  end
-end
-
-Event.register({defines.events.on_player_selected_area, defines.event.on_player_alt_selected_area}, measure_area)
+require('scripts/tool')
---
+++ scripts/tool.lua
@@ -0,0 +1,17 @@
+local Event = require('__stdlib__/stdlib/event/event')
+local Area = require('__stdlib__/stdlib/area/area')
+
+local function metrics(event)
+  if event.item == 'tile-metrics-tool' then
+    local player = game.players[event.player_index]
+    local area = Area(event.area)
+    if event.name == defines.events.on_player_alt_selected_area then
+      area = area:ceil()
+    end
+    local size, width, height = area:size()
+    player.print(area .. ' Center = ' .. area:center())
+    player.print('Size = ' .. size .. ' Width = ' .. width .. ' Height = ' .. height)
+  end
+end
+
+Event.register({defines.events.on_player_selected_area, defines.events.on_player_alt_selected_area}, metrics)
